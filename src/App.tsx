6250eeaac88d40ecf11ed1882168d9757551fd8c
snow <rhasya@gmail.com>
2025-05-15 13:23:40 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <main className="flex flex-col mx-auto w-[60%] items-start py-8 gap-2">
+      <div>This is Sample App</div>
+      <Button>CLICK</Button>
+    </main>
+  );
+}
